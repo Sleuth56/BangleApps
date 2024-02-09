990070947d5be5deda8866ee6f817c75a2bd37d5
Stephan Burns <sleuth56@protonmail.com>
2024-02-09 15:26:57 -0500

    Change how settings are handled

--- apps/step_goal/boot.js
+++ apps/step_goal/boot.js
@@ -1,40 +1,59 @@
 function step_goal_notif() {
+  var settings = Object.assign({
+    goal_enabled: true,
+    nag_enabled: true,
+    reminder_start_time: 9,
+    reminder_stop_time: 21
+  }, require("Storage").readJSON("step_goal.json", true) || {});
+
+  function setSettings() {
+    require("Storage").writeJSON("step_goal.json", settings);
+  }
+
   var health_settings = require("Storage").readJSON("health.json",1)||{};
-  var settings = require("Storage").readJSON("step_goal.json",1)||{};
   if (settings.has_triggered == undefined) {
-    write_to_config('has_triggered', false);
+    settings.has_triggered = false;
+    setSettings();
   }
   
   function achieved_step_goal() {
     load('step_goal.show_goal.js');
   }
-
-  function write_to_config(key, value) {
-    let json_file_data = require("Storage").readJSON("step_goal.json");
-    if (typeof(json_file_data) == undefined) {
-      json_file_data = {};
-    }
-    json_file_data[key] = value;
-    require("Storage").writeJSON("step_goal.json", json_file_data);
-  }
   
   function main() {
     if (!settings.has_triggered) {
       if (Bangle.getHealthStatus("day").steps >= health_settings.stepGoal) {
-        write_to_config('has_triggered', true);
+        settings.has_triggered = true;
+        setSettings();
         achieved_step_goal();
       }
     }
     else {
       if (Bangle.getHealthStatus("day").steps <= health_settings.stepGoal) {
-        write_to_config('has_triggered', false);
+        settings.has_triggered = false;
+        setSettings();
       }
     }
   }
 
-  Bangle.on('midnight', function() { write_to_config('has_triggered', false); });
+  Bangle.on('midnight', function() { step_goal_reset(); });
   main();
   setInterval(main, 60000);
+}
+
+function step_goal_reset() {
+  var settings = Object.assign({
+    goal_enabled: true,
+    nag_enabled: true,
+    reminder_start_time: 9,
+    reminder_stop_time: 21
+  }, require("Storage").readJSON("step_goal.json", true) || {});
+
+  function setSettings() {
+    require("Storage").writeJSON("step_goal.json", settings);
+  }
+  settings.has_triggered = false;
+  setSettings();
 }
 
 step_goal_notif();
