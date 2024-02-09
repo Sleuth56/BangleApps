d0b27630b7cd4ce20afd688a2976bad7a6068215
Stephan Burns <sleuth56@protonmail.com>
2024-02-09 17:21:01 -0500

    Respect settings when showing the goal display

--- apps/step_goal/boot.js
+++ apps/step_goal/boot.js
@@ -17,24 +17,33 @@ function step_goal_notif() {
   }
   
   function main() {
+    // Check if the message has been shown already
     if (!settings.has_triggered) {
-      if (Bangle.getHealthStatus("day").steps >= health_settings.stepGoal) {
+      // Check if we are at or over our step goal
+      if (Bangle.getHealthStatus("day").steps >= health_settings.stepGoal && settings.goal_enabled == true) {
+        // Trigger the message
         achieved_step_goal();
       }
     }
+    // If we have shown the screen
     else {
+      // Check if we aren't over our step goal
+      // Note: This code is for when someone changes their step goal after it has been reached for the day.
       if (Bangle.getHealthStatus("day").steps <= health_settings.stepGoal) {
+        // Set has_triggered to false so the screen can run once the goal is reached.
         settings.has_triggered = false;
         setSettings();
       }
     }
   }
 
+  // Make it so we run the function that resets the has_triggered var so we can run again the next day.
   Bangle.on('midnight', function() { step_goal_reset(); });
   main();
   setInterval(main, 60000);
 }
 
+// Resets the has_triggered step goal
 function step_goal_reset() {
   var settings = Object.assign({
     goal_enabled: true,
