be549f0b8a21fb8e7069776fb069fad7cbef65c6
Stephan Burns <sleuth56@protonmail.com>
2024-02-28 10:08:40 -0500

    We now persist our previous value between reloads

--- apps/global_hrm/boot.js
+++ apps/global_hrm/boot.js
@@ -1,7 +1,7 @@
 global.hrm = Object.assign({
-    bpm: undefined,
-    confidence: 0
-});
+  bpm: undefined,
+  confidence: 0
+}, require("Storage").readJSON("global_hrm.json", true) || {});
 
 {
   const oldSetHRMPower = Bangle.setHRMPower;
@@ -12,14 +12,24 @@ global.hrm = Object.assign({
     disable_HRM_lockout: false
   }, require("Storage").readJSON("global_hrm.json", true) || {});
 
+  let setSettings = () => {
+    require("Storage").writeJSON("global_hrm.json", settings);
+  };
+
   let updateHrm = () => {
     oldSetHRMPower(true, "global_hrm");
     function _get_HRM(data) {
-      if (data.confidence > settings.bpm_confidence) {
-        global.hrm.bpm = data.bpm;
-        global.hrm.confidence = data.confidence;
-        console.log(`Global HRM Update BPM: ${data.bpm} confidence: ${data.confidence}`);
-        oldSetHRMPower(false, "global_hrm");
+      if (data.confidence > settings.bpm_confidence && data.bpm > 0) {
+        if (data.bpm != global.hrm.bpm && data.confidence != global.hrm.confidence) {
+          global.hrm.bpm = data.bpm;
+          global.hrm.confidence = data.confidence;
+          settings.bpm = data.bpm;
+          settings.confidence = data.confidence;
+          console.log(`Global HRM Update BPM: ${data.bpm} confidence: ${data.confidence}`);
+          setSettings();
+        }
+      oldSetHRMPower(false, "global_hrm");
+      setInterval(updateHrm, settings.update_interval_in_minutes*60*1000);
       }
     }
     Bangle.on('HRM',_get_HRM);
@@ -27,12 +37,11 @@ global.hrm = Object.assign({
 
   if (settings.enabled) {
     updateHrm();
-    setInterval(updateHrm, settings.update_interval_in_minutes*60*1000);
   }
 
   // Disable setHRMPower so no other programs can use it
   Bangle.setHRMPower = function(on, id) {
-    if (settings.enabled && settings.disable_HRM_lockout) {
+    if (settings.enabled && !settings.disable_HRM_lockout) {
         return on;
     }
     else {
