5d38710331e1cbd53edb448cf5e43a677bde07af
Stephan Burns <sleuth56@protonmail.com>
2024-04-05 15:03:09 -0400

    Remove fast reload

--- apps/step_goal/show_reminder.js
+++ apps/step_goal/show_reminder.js
@@ -1,29 +1,29 @@
 {
-  img1 = function() {
+  let img1 = function() {
     return require("heatshrink").decompress(atob("mEw4cA///hFC1P930kkX65FqtGLtFSFKsSpAQOgmSpIRRkARNkmbyUkBY4aFm8kyXEA4eSJwMBDQspCINEHggROBgIRChIRF5ILBrAFBBgOpIgIRGpIdDAoQOCOgwJByVAUoIMBCJxEEgSqHkmQgE0CgQRMhu2yQrDHQIQFgOkyVN23QHojyGtoJBtu24ARKgYRD2BhFCIsbtu0zdtwC0EPgwRB23bthODCJHbCOhWBCJ+TCKCeCCPTEBpMpkgRMpMipMlkjNCCJEBlIRDdQgRGhMpgVJkgUBCJQOBAQMgCgJbDyRjCKIQODCgYkDCI0AiQGBwEJCL0qNYNACJZ3BCKMOpNv4EBCJkJ7mSoARNdQQDBCJQACgjVECJYAIkmQCKMJCOQAgA=="));
   };
-  img2 = function() {
+  let img2 = function() {
     return require("heatshrink").decompress(atob("mEw4cA///1P9yd85X9vN8pP8+ms1P7KPsZlgRP7dtwAR/CKxrRAEU30mTmwQMgO3kmSm3ACJcN3oRBs3YCJdt3IRBo3bCBUDtvpCINM7dgCJMbtvJCINI7dsWBVspIRCC4K2IgwvBCIY7B2ARHmxTBCIZfB2wPFgVLmx3BCIkN2+SoAREpO2TYIREgO26QRFoIsCr////6HwXCCIoFDCIoLFCNe/CJMAMgIRC/oREiSQFCM7aBhrUDCI0JCJOQCLEgCIXYCIcECI0BCJOACI0kCIcMCIUkCIwIBCIYIGAAosCCIg+DCIxQBCIhiDAA2SCItJCBCqBoARDgS8DAA0CpMCrApCoARJF4JlCkg7BABSSByQUBRg5cHKxQAh"));
   };
-  img3 = function() {
+  let img3 = function() {
     return require("heatshrink").decompress(atob("mEw4UA///5X9vEF9dSIW0BqgLJgtVBZMVqtABdMBHAI7ICgNaKZFVqtq0BnHqsFtWqEQ9QgGqBZFAgWq1ZGGisABYOpLxEK1WtE4ILI0oLIlWq2ILKwK/IKQRvBDIsD9QLK/2oSYQLFh//9C8IBYPwBZE///8BZH/AAILL4AKGgf/4YwGdINXEIIwGLANVBAM/BZFwKoPVEY1VY4LyBJBALBUxAcCBZbKIIAQLLHwIAIJQQLJKRReWgBGKAFQA=="));
   };
 
-  buz_watch = function() {
+  let buz_watch = function() {
     Bangle.buzz(1000, 0.1);
   };
 
-  steps_per_hour = function(step_goal, start_hour, end_hour) {
+  let steps_per_hour = function(step_goal, start_hour, end_hour) {
     return Math.ceil(step_goal / (end_hour-start_hour));
   };
 
-  needed_steps = function(current_steps, step_goal, start_hour, end_hour, current_hour) {
+  let needed_steps = function(current_steps, step_goal, start_hour, end_hour, current_hour) {
     let _steps = steps_per_hour(step_goal, start_hour, end_hour);
     let steps_needed_at_this_hour = _steps*(current_hour-start_hour);
     return steps_needed_at_this_hour - current_steps;
   };
 
-  display_graphic = function() {
+  let display_graphic = function() {
     var settings = Object.assign({
       goal_enabled: true,
       reminder_enabled: true,
@@ -32,9 +32,9 @@
       has_triggered: false
     }, require("Storage").readJSON("step_goal.json", true) || {});
 
-    function setSettings() {
+    let setSettings = function() {
       require("Storage").writeJSON("step_goal.json", settings);
-    }
+    };
     var health_settings = require("Storage").readJSON("health.json",1)||{};
     const timeout = 10000;
     // Random options
@@ -70,8 +70,9 @@
 
     // Wait for our timeout than return to clock.
     return new Promise(resolve=>setTimeout(resolve,timeout)).then(()=>{
-    Bangle.setOptions({backlightTimeout: require("Storage").readJSON("setting.json",1).timeout});
-    eval(require("Storage").read(require("Storage").readJSON("setting.json",1).clock));
+      // Bangle.setOptions({backlightTimeout: require("Storage").readJSON("setting.json",1).timeout*1000});
+      // eval(require("Storage").read(require("Storage").readJSON("setting.json",1).clock));
+      load();
     });
   };
 
